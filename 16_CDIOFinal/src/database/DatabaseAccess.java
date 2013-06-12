ab07cb168ee83f85d2adc4ed66bbf1e28e1a4653
Jens Werner Nielsen <jenswernernielsen@gmail.com>
2013-06-12 11:40:42 +0200

    Rettet navn

--- 16_CDIOFinal/src/database/DatabaseAccess.java
+++ 16_CDIOFinal/src/database/DatabaseAccess.java
@@ -86,7 +86,7 @@ public class DatabaseAccess {
 			// Create
 			connector.doUpdate("CREATE TABLE operator(oprId INTEGER NOT NULL AUTO_INCREMENT, oprName VARCHAR(20), ini VARCHAR(4), cpr VARCHAR(10) NOT NULL, Upassword VARCHAR(10) NOT NULL, rights INTEGER NOT NULL, PRIMARY KEY(oprId)) ENGINE=innoDB;");
 			connector.doUpdate("CREATE TABLE commodity(commodityId INTEGER NOT NULL AUTO_INCREMENT, commodityName VARCHAR(20), supplier VARCHAR(20), PRIMARY KEY(commodityId)) ENGINE=innoDB;");
-			connector.doUpdate("CREATE TABLE commodityBatch(cbId INTEGER NOT NULL AUTO_INCREMENT, commodityId INTEGER, maengde REAL NOT NULL, PRIMARY KEY(cbId), FOREIGN KEY (commodityId) REFERENCES commodity(commodityId)) ENGINE=innoDB;");
+			connector.doUpdate("CREATE TABLE commodityBatch(cbId INTEGER NOT NULL AUTO_INCREMENT, commodityId INTEGER, quantity REAL NOT NULL, PRIMARY KEY(cbId), FOREIGN KEY (commodityId) REFERENCES commodity(commodityId)) ENGINE=innoDB;");
 			connector.doUpdate("CREATE TABLE recipe(recipeId INTEGER NOT NULL AUTO_INCREMENT, recipeName VARCHAR(20), PRIMARY KEY(recipeId)) ENGINE=innoDB;");
 			connector.doUpdate("CREATE TABLE recipeComponent(recipeId INTEGER, commodityId INTEGER, nomNetto REAL NOT NULL, tolerance REAL NOT NULL, PRIMARY KEY(recipeId, commodityId), FOREIGN KEY(recipeId) REFERENCES recipe(recipeId), FOREIGN KEY(commodityId) REFERENCES commodity(commodityId)) ENGINE=innoDB;");
 			connector.doUpdate("CREATE TABLE productBatch(pbId INTEGER NOT NULL AUTO_INCREMENT, recipeId INTEGER, ts DATETIME, state INTEGER NOT NULL, PRIMARY KEY(pbId), FOREIGN KEY(recipeId) REFERENCES recipe(recipeId)) ENGINE=innoDB;");
